ec58500894524ada2af59953fa447007bd532f45
Hai Luu <luuph@bsscommerce.com>
2025-02-11 13:29:30 +0700

    Update admin link

--- app/code/Chill/Vortex/Block/AdminLink.php
+++ app/code/Chill/Vortex/Block/AdminLink.php
@@ -2,12 +2,24 @@
 namespace Chill\Vortex\Block;
 
 use Magento\Framework\View\Element\Html\Link;
+use Magento\Backend\Helper\Data as BackendHelper;
+use Magento\Framework\View\Element\Template;
 
 class AdminLink extends Link
 {
+    protected $backendHelper;
+
+    public function __construct(
+        Template\Context $context,
+        BackendHelper $backendHelper,
+        array $data = []
+    ) {
+        $this->backendHelper = $backendHelper;
+        parent::__construct($context, $data);
+    }
     public function getHref()
     {
-        return $this->getUrl('admin'); // URL sẽ dẫn đến trang admin
+        return $this->backendHelper->getHomePageUrl();
     }
 
     public function getLabel()
